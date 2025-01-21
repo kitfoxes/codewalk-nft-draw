23568d6de57227d5a0daefa627a71d415e588e8b
suyogkarki <karkisuyog659@gmail.com>
2025-01-21 18:49:53 +0545

    draw

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Excalidraw } from "@excalidraw/excalidraw";
+function App() {
+  return (
+    <div style={{height:"100vh"}}>
+      <Excalidraw/>
+    </div>
+  );
+}
+
+export default App;
